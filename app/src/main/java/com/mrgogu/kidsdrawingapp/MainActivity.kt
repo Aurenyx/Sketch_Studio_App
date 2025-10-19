f8a7b76ec7829f3e7031f87852ca3a0891375a1c
Aurenyx <govindgulhane5@gmail.com>
2025-10-19 21:03:26 +0530

    edit background image view

--- app/src/main/java/com/mrgogu/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/mrgogu/kidsdrawingapp/MainActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
