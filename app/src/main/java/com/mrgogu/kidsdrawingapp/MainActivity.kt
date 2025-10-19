67d79b25162d854670ee6ce18714caac933825cb
Aurenyx <govindgulhane5@gmail.com>
2025-10-19 21:05:09 +0530

    accessing media

--- app/src/main/java/com/mrgogu/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/mrgogu/kidsdrawingapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.app.Dialog
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.ImageButton
